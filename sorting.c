d030b0c924cfb47989bac3a321e293523131c968
ayush-Sharma173 <ayush.deepak173@gmail,com>
2022-07-27 19:17:56 +0530

    merge sort

--- sorting.c
+++ sorting.c
@@ -26,6 +26,70 @@ void insertion_sort()
 }
 
 
+void merge(int p, int q, int r) {
+
+  // Create L ← A[p..q] and M ← A[q+1..r]
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  int L[n1], M[n2];
+
+  for (int i = 0; i < n1; i++)
+    L[i] = array[p + i];
+  for (int j = 0; j < n2; j++)
+    M[j] = array[q + 1 + j];
+
+  // Maintain current index of sub-arrays and main array
+  int i, j, k;
+  i = 0;
+  j = 0;
+  k = p;
+
+  // Until we reach either end of either L or M, pick larger among
+  // elements L and M and place them in the correct position at A[p..r]
+  while (i < n1 && j < n2) {
+    if (L[i] <= M[j]) {
+      array[k] = L[i];
+      i++;
+    } else {
+      array[k] = M[j];
+      j++;
+    }
+    k++;
+  }
+
+  // When we run out of elements in either L or M,
+  // pick up the remaining elements and put in A[p..r]
+  while (i < n1) {
+    array[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    array[k] = M[j];
+    j++;
+    k++;
+  }
+}
+
+// Divide the array into two subarrays, sort them and merge them
+void mergeSort(int l, int r) {
+  if (l < r) {
+
+    // m is the point where the array is divided into two subarrays
+    int m = l + (r - l) / 2;
+
+    mergeSort(l, m);
+    mergeSort(m + 1, r);
+
+    // Merge the sorted subarrays
+    merge(l, m, r);
+  }
+}
+
+
+
 void selection_sort()
 {
     for (c = 0; c < (n - 1); c++) // finding minimum element (n-1) times
@@ -81,6 +145,7 @@ int main()
         printf("\nPress 1 for insertion sort");
         printf("\nPress 2 for selection sort");
         printf("\nPress 3 for bubble sort");
+        printf("\nPress 4 for merge sort");
             printf("\n\n");
             int algo;
 
@@ -104,6 +169,12 @@ int main()
             {
                 bubble_sort();
                 printf("Sorted list in ascending order (using bubble sort):\n");
+            }
+
+            if(algo==4)
+            {
+                mergeSort(0,n-1);
+                printf("Sorted list in ascending order (using merge sort):\n");
             }
 
         for (c = 0; c <= n - 1; c++) {
